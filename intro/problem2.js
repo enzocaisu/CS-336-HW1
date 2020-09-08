5f546a26531372be11ec0204bd31b5018ffd5410
enzoca <enzoca@iastate.edu>
2020-09-07 20:53:34 -0500

    Initial commit of CS336 HW1

---
+++ intro/problem2.js
@@ -0,0 +1,114 @@
+// Same as GL_example1a but uses a uniform variable in the vertex
+// shader to control the left or right shift of the model.  The shift is
+// updated in each frame (see the bottom of the main method) to animate
+// the model.
+
+// Raw data for some point positions - this will be a square, consisting
+// of two triangles.  We provide two values per vertex for the x and y coordinates
+// (z will be zero by default).
+
+// A few global variables...
+var vertices = new Float32Array([
+  -0.5, -0.5,
+  0.5, -0.5,
+  0.5, 0.5,
+  -0.5, 0.5
+])
+
+
+// the OpenGL context
+var gl;
+
+// handle to a buffer on the GPU
+var vertexbuffer;
+
+// handle to the compiled shader program on the GPU
+var shader;
+
+// code to actually render our geometry
+function draw(numSides)
+{
+  // clear the framebuffer
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // bind the shader
+  gl.useProgram(shader);
+
+  //create the new vertices
+  var newVertices = [0.0, 0.0];
+  var theta = (2 * Math.PI) / numSides;
+  for(var i = 0; i < numSides; i++){
+    newVertices.push(Math.cos(i * theta));
+    console.log(Math.cos(i*theta));
+    newVertices.push(Math.sin(i * theta));
+    console.log(Math.sin(i*theta));
+  }
+
+  //bind the vertices to the buffer
+  vertexbuffer = createAndLoadBuffer(newVertices);
+  // bind the buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexbuffer);
+
+  // get the index for the a_Position attribute defined in the vertex shader
+  var positionIndex = gl.getAttribLocation(shader, 'a_Position');
+  if (positionIndex < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // "enable" the a_position attribute
+  gl.enableVertexAttribArray(positionIndex);
+
+  // associate the data in the currently bound buffer with the a_position attribute
+  // (The '2' specifies there are 2 floats per vertex in the buffer.  Don't worry about
+  // the last three args just yet.)
+  gl.vertexAttribPointer(positionIndex, 2, gl.FLOAT, false, 0, 0);
+
+  // we can unbind the buffer now (not really necessary when there is only one buffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  // draw, specifying the type of primitive to assemble from the vertices
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, numSides);
+
+  // unbind shader and "disable" the attribute indices
+  // (not really necessary when there is only one shader)
+  gl.disableVertexAttribArray(positionIndex);
+  gl.useProgram(null);
+
+}
+
+// entry point when page is loaded
+function main() {
+
+  // basically this function does setup that "should" only have to be done once,
+  // while draw() does things that have to be repeated each time the canvas is
+  // redrawn
+
+    // get graphics context
+  gl = getGraphicsContext("theCanvas");
+
+  // load and compile the shader pair
+  shader = createProgram(gl, 'vertexShader', 'fragmentShader');
+
+  // load the vertex data into GPU memory
+  vertexbuffer = createAndLoadBuffer(vertices);
+
+  // specify a fill color for clearing the framebuffer
+  gl.clearColor(0.0, 0.8, 0.8, 1.0);
+
+  // we could just call draw() once to see the result, but setting up an animation
+  // loop to continually update the canvas makes it easier to experiment with the
+  // shaders
+  //draw();
+
+  let text = document.getElementById("sidesBox");
+  let sides = text.value;
+
+  var getSides = function(){
+    sides = text.value;
+    draw(sides);
+  }
+
+  draw(sides);
+  text.onchange = getSides();
+}
